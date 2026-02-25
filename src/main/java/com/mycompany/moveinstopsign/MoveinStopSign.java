4765071c2f0c8208aff42b4ee414812ca5747ab3
jade1104 <ST10503964@myemeris.edu.za>
2026-02-25 14:54:29 +0200

    final submission

---
+++ src/main/java/com/mycompany/moveinstopsign/MoveinStopSign.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+// A simple application that alternates between a stop and go sign
+
+package com.mycompany.moveinstopsign;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class MoveinStopSign {
+
+   public static void main(String[] args) throws InterruptedException {
+  
+        //infinate loop of stop signs...
+        while(true) {
+        System.out.println("                                ");
+        System.out.println("                                ");
+        System.out.println("                                ");
+        System.out.println("        %%%%%%%%                ");
+        System.out.println("      %%        %%             ");
+        System.out.println("    %|%          %|%            ");
+        System.out.println("    %|%   STOP   %|%            ");
+        System.out.println("    %|%          %|%            ");
+        System.out.println("     %%         %%              ");
+        System.out.println("        %%%%%%%                 ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("       *********                ");
+        System.out.println("     *************              ");
+        System.out.println("|-------------------------------");
+        System.out.println("|-------------------------------");
+        //1000ms break in the compute
+        Thread.sleep(1000);
+        
+        System.out.println("                                ");
+        System.out.println("                                ");
+        System.out.println("                                ");
+        System.out.println("        %%%%%%%%                ");
+        System.out.println("      %%        %%             ");
+        System.out.println("    %/%          %/%            ");
+        System.out.println("    %/%    GO    %/%            ");
+        System.out.println("    %/%          %/%            ");
+        System.out.println("     %%         %%              ");
+        System.out.println("        %%%%%%%                 ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("          ***                   ");
+        System.out.println("       *********                ");
+        System.out.println("     *************              ");
+        System.out.println("-------------------------------|");
+        System.out.println("-------------------------------|");
+        //1000ms break in the compute
+        Thread.sleep(1000);
+        }
+    }
+}
+    
+
